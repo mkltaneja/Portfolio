4ba4792bf6a6a8d08457d4af8c01f64447d35c0e
BirdieMukul <mukul.taneja@birdeye.com>
2023-11-18 20:59:06 +0530

    Phase 1 completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar/navbar";
 import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
+import Contact from "./components/Contact/contact";
+import Footer from "./components/Footer/footer";
 import './style.css';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Intro />
       <Skills />
       <Works />
+      <Contact />
+      <Footer />
     </div>
   );
 }
